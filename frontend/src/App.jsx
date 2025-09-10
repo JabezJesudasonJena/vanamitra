474f1ae15a08dd0cb4c82aa3c26594a732852fc6
JabezJesudasonJena <boycalledjabez@gmail.com>
2025-09-10 23:16:03 +0530

    Added Map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import React from "react"
 
 function App() {
-
   return (
     <div>
-      
+      <div class="flex flex-col">
+        <h1 className="text-bold">
+          Hello World 
+        </h1>
+      </div>
     </div>
-
   )
 }
 
