26f169c6ded7c5ffd15520e4a63418d217fa84a9
JabezJesudasonJena <boycalledjabez@gmail.com>
2025-09-15 15:53:31 +0530

    Set the current location

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -7,7 +7,7 @@ import MakerClusterGroup from "react-leaflet-markercluster"
 const markers = [
   {
     geocode: [48.86, 2.3522],
-    popUp: "Hello, I am pop up 1"
+    popUp: "This  is the note of the pin 1"
   },
   {
     geocode: [48.85, 2.3522],
@@ -22,7 +22,7 @@ const markers = [
 function Map() {
   return (
     <div>
-      <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+      <MapContainer center={[13.091010, 79.972741]} zoom={13}>
         <TileLayer
             attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contribxutors'
             url = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
