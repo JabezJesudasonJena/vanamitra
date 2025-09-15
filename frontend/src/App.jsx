6333b67c438165d375bbc72c18375c5c4cc38efe
JabezJesudasonJena <170162059+JabezJesudasonJena@users.noreply.github.com>
2025-09-15 05:39:43 +0530

    Added a shape drawing map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import ComplaintForm from "./ComplaintForm.jsx";
 import Map from './Map'
+import Mapwd from "./Mapwd.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/map" element={<Map />} />
         <Route path="/form" element={<ComplaintForm />} />
+        <Route path="/imap" element={<Mapwd />} />
       </Routes>
     </Router>
   );
