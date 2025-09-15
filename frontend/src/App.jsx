bb0901f5be4b6009ec89aee68a34e3db0a755f94
JabezJesudasonJena <boycalledjabez@gmail.com>
2025-09-15 15:43:56 +0530

    Removed the unessacry error causing files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import ComplaintForm from "./ComplaintForm.jsx";
 import Map from './Map'
-import Mapwd from "./Mapwd.jsx";
-import Mapd from "./assets/Mapd.jsx";
-
 const App = () => {
   return (
     <Router>
@@ -13,8 +10,6 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/map" element={<Map />} />
         <Route path="/form" element={<ComplaintForm />} />
-        <Route path="/imap" element={<Mapwd />} />
-        <Route path="/umap" element={<Mapd />} />
       </Routes>
     </Router>
   );
